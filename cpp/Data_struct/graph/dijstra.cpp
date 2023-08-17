dbd1e32223b54d6291834a7e2d3c5a3e425ca80b
Qunzhang1996 <QUNZ@CHALMERS.SE>
2023-08-17 17:25:21 +0200

    update

---
+++ cpp/Data_struct/graph/dijstra.cpp
@@ -0,0 +1,150 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+
+struct Point
+{
+    int x,y;
+    Point(int x, int y):x(x),y(y){};
+};
+struct Node
+{
+    Point point;
+    double g;
+    Node* parent;
+    Node(Point& point, int g, Node* parent=nullptr):point(point),g(g),parent(parent){};
+};
+struct Nodecomparison
+{
+    bool operator()(Node* Point1, Node* Point2)
+    {
+        return Point1->g>Point2->g;
+    }
+};
+vector<Point>find_neighour(const Point& point, const vector<vector<int>> map)
+{
+    int x=point.x;
+    int y=point.y;
+    vector<Point>neighbour_nodes;
+
+    for (int i=x-1;i<=x+1;i++)
+    {
+        for(int j=y-1;j<=y+1;j++)
+        {
+            if(i==x&&j==y) continue;
+            if(i >= 0 && j >= 0 && i < map.size() && j < map[0].size() && map[i][j] == 0)
+            {
+                neighbour_nodes.push_back({i,j});
+            }
+        }
+    }
+    return neighbour_nodes;
+};
+double calculate_distance(Point& point1, Point& point2)
+{
+    double distance = sqrt(pow(point1.x - point2.x, 2) + pow(point1.y - point2.y, 2));
+    const double DIAGONAL_BIAS = 0.001;
+
+    // Check if the movement is diagonal
+    if (abs(point1.x - point2.x) == 1 && abs(point1.y - point2.y) == 1) 
+    {
+        distance += DIAGONAL_BIAS;
+    }
+    return distance;
+}
+
+vector<Point> dijkstra(Point& start, Point& end, vector<vector<int>> map)
+{
+    vector<Node*>openlist;
+    vector<Node*>closelist;
+    vector<Point>path;
+    Node* current=new Node(start, 0, nullptr);
+    openlist.push_back(current);
+    while (!openlist.empty())
+    {
+        sort(openlist.begin(),openlist.end(),Nodecomparison());
+        current=*openlist.begin();
+        openlist.erase(openlist.begin());
+        closelist.push_back(current);
+        if(current->point.x==end.x&&current->point.y==end.y)
+        {
+            Node* terminal=current;
+            while(terminal->parent!=nullptr)
+            {
+                path.push_back(terminal->point);
+                terminal=terminal->parent;
+            };
+            path.push_back(start);
+            reverse(path.begin(),path.end());
+            return path;
+        };
+        vector<Point>neighbour_nodes=find_neighour(current->point, map);
+        for(auto neigh:neighbour_nodes)
+        {
+            bool incloselist = false;
+            for(auto c:closelist)
+            {
+                if(c->point.x==neigh.x&&c->point.y==neigh.y) 
+                {
+                    incloselist=true;
+                    break;
+                }
+            };
+            if(incloselist) continue;
+
+            bool inopenlist = false;
+            for(auto o:openlist)
+            {
+                if(o->point.x==neigh.x&&o->point.y==neigh.y)
+                {
+                    inopenlist=true;
+                    double g=current->g+calculate_distance(neigh,current->point);
+                    if(g<o->g)
+                    {
+                        o->g=g;
+                        o->parent=current;
+                    }
+                }
+            };
+            if(!inopenlist)
+            {
+                double g=current->g+calculate_distance(neigh,current->point);
+                openlist.push_back(new Node(neigh,g,current));
+            };
+        }
+    }
+    
+    for(auto o:openlist) delete o;
+    for(auto c:closelist) delete c;
+    delete current;
+    return {};
+    
+};
+
+int main() {
+    vector<vector<int>> map = {
+        {0, 1, 0, 0, 1},
+        {1, 0, 0, 0, 0},
+        {0, 1, 0, 1, 0},
+        {0, 1, 1, 0, 1},
+        {1, 1, 0, 1, 0}
+    };
+
+    Point start(0, 0);
+    Point end(2, 4);
+
+    vector<Point> path = dijkstra(start, end, map);
+
+    if(path.empty()) {
+        cout << "No path found!" << endl;
+    } else {
+        for (const auto& point : path) {
+            cout << "(" << point.x << ", " << point.y << ") -> ";
+        }
+        cout << "END" << endl;
+    }
+
+    return 0;
+}
