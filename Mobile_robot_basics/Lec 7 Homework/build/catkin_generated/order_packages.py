5e82e27809f181857b885fcc19664b5871f8b9ec
Qunzhang1996 <QUNZ@CHALMERS.SE>
2023-09-17 00:10:01 +0200

    update

--- Mobile_robot_basics/Lec 7 Homework/build/catkin_generated/order_packages.py
+++ Mobile_robot_basics/Lec 7 Homework/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/zq/C-programe-practise/Mobile_robot_basics/Lec 7 Homework/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/zq/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/zq/catkin_ws/devel;/opt/ros/noetic' != '' else []
--- Mobile_robot_basics/Lec 7 Homework/build/catkin_generated/setup_cached.sh
+++ Mobile_robot_basics/Lec 7 Homework/build/catkin_generated/setup_cached.sh
@@ -8,6 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/zq/C-programe-practise/Mobile_robot_basics/Lec 7 Homework/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/zq/C-programe-practise/Mobile_robot_basics/Lec 7 Homework/devel/lib:/home/zq/catkin_ws/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu'
+export PATH='/opt/ros/noetic/bin:/home/zq/anaconda3/condabin:/home/zq/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/path_to_pycharm_directory/bin'
+export PKG_CONFIG_PATH='/home/zq/catkin_ws/devel/lib/pkgconfig:/opt/ros/noetic/lib/pkgconfig:/opt/ros/noetic/lib/x86_64-linux-gnu/pkgconfig'
 export PWD='/home/zq/C-programe-practise/Mobile_robot_basics/Lec 7 Homework/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/zq/C-programe-practise/Mobile_robot_basics/Lec 7 Homework/devel/share/common-lisp'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/zq/C-programe-practise/Mobile_robot_basics/Lec 7 Homework/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/zq/C-programe-practise/Mobile_robot_basics/Lec 7 Homework/src:$ROS_PACKAGE_PATH"
